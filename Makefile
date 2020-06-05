ad4a450dd81973ff47d21e48ab946ceb7ae0b179
himselfv <me@boku.ru>
2020-06-05 16:08:30 +0300

    Upgraded makefile to build everything into subfolders of /build

--- Makefile
+++ Makefile
@@ -1,15 +1,21 @@
 
 min.js: js
-	.\node_modules\.bin\minify ./tmp/index.js --out-file ./tmp/index.min.js
+	.\node_modules\.bin\minify ./build/min/index.js --out-file ./build/min/index.min.js
 
 js: build
-#	-rm -rf /tmp/index.js
-	.\node_modules\.bin\browserify --standalone index .\build\index.js \
+	-rm -rf ./build/min/
+	.\node_modules\.bin\browserify --standalone index .\build\base\index.js \
                 -t [ babelify --presets [ @babel/preset-env ] ] \
-                --outfile .\tmp\index.js
+                --outfile .\build\min\index.js
 
 build:
+	-rm -rf ./build/base/
 	npm run build
-#	rm -rf build/
-#	.\node_modules\.bin\babel src --out-dir build
 
+clean:
+	-rm -rf ./build/*
+	# Remove unrelated
+	-rm -rf ./build/base/dav/ical-readme.txt
+	-rm -rf ./build/base/dav/README.md
+	# Avoid building with personalized config.jses
+	-rm -rf ./build/base/dav/config.js
--- src/config-example.js
+++ src/config-example.js
@@ -1,6 +1,14 @@
 /*
-Client ID and API key from the Developer Console
+Options are set from the UI but the defaults can be changed:
+*/
+options = options || {};
+options.noMergeByDelete = false; //Disable: Merge the next task into this one by Delete button
+options.noMergeByBackspace = false; //Disable: Merge this task into the previous one by Backspace button
+options.singleClickAdd = false; //Add new task with a single click on the empty space - as it had been in GTasks. Double click always works.
+options.debug = false; //Enables debug backends and more logging
 
+/*
+GTasks backend: Client ID and API key from the Developer Console
 Register your app here:
 https://console.developers.google.com/cloud-resource-manager
 See here how:
@@ -9,9 +17,3 @@ https://developers.google.com/tasks/firstapp
 var GTASKS_CLIENT_ID = '';
 var GTASKS_API_KEY = '';
 */
-
-
-options.noMergeByDelete = false; //Disable: Merge the next task into this one by Delete button
-options.noMergeByBackspace = false; //Disable: Merge this task into the previous one by Backspace button
-options.singleClickAdd = false; //Add new task with a single click on the empty space - as it had been in GTasks. Double click always works.
-options.debug = false; //Enables debug backends and more logging
